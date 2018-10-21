387995f7876c2021904cf90223675290185e1380
Fernanda Duarte <fernanda.aduarte@hotmail.com>
2018-10-21 15:13:24 -0300

    Changes Header

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 import style from './style.module.scss'
 import logo from '../../resources/logo-bitUs-verde.png'
 
 const BePartOfIt = () => (
   <header className={style.header}>
-    <img className={style.img} src={logo} alt="logo" />
+    <Link
+      to="/"
+      aria-hidden
+      className={style.link}
+    >
+      <img className={style.img} src={logo} alt="logo" />
+    </Link>
   </header>
 )
 
