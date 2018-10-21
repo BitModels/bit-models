151dc2993c6dbace10f7d67f1cefe51313653c82
Fernanda Duarte <fernanda.aduarte@hotmail.com>
2018-10-21 16:41:51 -0300

    Changes Profile page

--- src/pages/profiles/index.jsx
+++ src/pages/profiles/index.jsx
@@ -1,18 +1,48 @@
 import React from 'react'
+import Icon from '@material-ui/core/Icon'
 
+import profiles from '../../testData/profiles'
 import style from './style.module.scss'
-import logo from '../../resources/logo-bitUs-verde.png'
+import NavButton from '../../components/navButton'
+import SearchHeader from '../../components/searchHeader'
 
 class Profiles extends React.Component {
+  handleSearch = (e) => {
+    console.log(e)
+  }
+
+  renderProfileButton = profile => (
+    <div key={profile.id} className={style.itemContainer}>
+      <div className={style.iconContainer}>
+        <Icon className={style.icon}>computer</Icon>
+      </div>
+      <div className={style.dataContainer}>
+        <span className={style.name}>
+          {profile.name}
+        </span>
+        <span>
+          {profile.description}
+        </span>
+      </div>
+    </div>
+  )
+
   render() {
     return (
       <div>
-        <header>
-          <img src={logo} alt="logo" />
-        </header>
-        <div>
-        Profiles
-        </div>
+        <SearchHeader
+          onSearch={this.handleSearch}
+        />
+        <nav className={style.nav}>
+          <ul className={style.ul}>
+            <NavButton label="INÍCIO" to="/" />
+            <NavButton label="FAÇA PARTE" to="/facaParte" />
+            <NavButton label="SOBRE" to="/sobre" />
+          </ul>
+        </nav>
+        <main className={style.main}>
+          {profiles.map(p => this.renderProfileButton(p))}
+        </main>
       </div>
     )
   }
---
+++ src/testData/profiles.jsx
@@ -0,0 +1,44 @@
+export default [
+  {
+    id: 0,
+    name: 'Fernanda Duarte',
+    description: 'Desenvolvedora de aplicativos de celular e sistemas web',
+    image: 'https://www.yu.edu/sites/default/files/ThinkstockPhotos-658148844.jpg',
+  },
+  {
+    id: 1,
+    name: 'Daniela Alves',
+    description: 'Cientista de Dados',
+    image: 'https://www.yu.edu/sites/default/files/ThinkstockPhotos-658148844.jpg',
+  },
+  {
+    id: 2,
+    name: 'Maria Letícia',
+    description: 'Analista de software',
+    image: 'https://www.yu.edu/sites/default/files/ThinkstockPhotos-658148844.jpg',
+  },
+  {
+    id: 3,
+    name: 'Angélica Silva',
+    description: 'Pesquisadora',
+    image: 'https://www.yu.edu/sites/default/files/ThinkstockPhotos-658148844.jpg',
+  },
+  {
+    id: 4,
+    name: 'Fernanda Duarte',
+    description: 'Desenvolvedora de aplicativos de celular e sistemas web',
+    image: 'https://www.yu.edu/sites/default/files/ThinkstockPhotos-658148844.jpg',
+  },
+  {
+    id: 5,
+    name: 'Fernanda Duarte',
+    description: 'Desenvolvedora de aplicativos de celular e sistemas web',
+    image: 'https://www.yu.edu/sites/default/files/ThinkstockPhotos-658148844.jpg',
+  },
+  {
+    id: 6,
+    name: 'Fernanda Duarte',
+    description: 'Desenvolvedora de aplicativos de celular e sistemas web',
+    image: 'https://www.yu.edu/sites/default/files/ThinkstockPhotos-658148844.jpg',
+  },
+]
