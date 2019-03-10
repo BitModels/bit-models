cc265a23b87d7e9c1cbea9ef224de6f5b16aff98
Fernanda Duarte <fernanda.aduarte@hotmail.com>
2019-03-10 09:54:58 -0300

    Start "Be part" form

--- src/pages/bePartOfIt/index.jsx
+++ src/pages/bePartOfIt/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import TextField from '@material-ui/core/TextField'
 
 import style from './style.module.scss'
 import Header from '../../components/header'
@@ -23,11 +24,51 @@ const BePartOfIt = () => (
         },
       ]}
     />
-    <main className={style.contentContainer}>
-      <p className={style.p}>
-      Em breve você poderá inserir seu perfil no BitUs. Aguarde essa nova funcionalidade!
-      </p>
-    </main>
+    <div className={style.contentContainer}>
+      <main>
+        <h1 className={style.title}>
+      Faça parte
+        </h1>
+        <p className={style.description}>Quer ver o seu perfil publicado no BitUs? Conte-nos sobre você no formulário abaixo!</p>
+        <form noValidate autoComplete="off" className={style.formContainer}>
+          <TextField
+            id="name"
+            label="Nome"
+          />
+          <TextField
+            id="location"
+            label="Localização"
+          />
+          <TextField
+            id="location"
+            label="Educação"
+            placeholder="ex.: Faculdade, pós-graduacão, cursos, etc"
+          />
+          <TextField
+            id="about"
+            label="Sobre"
+            multiline
+            rows={5}
+            variant="outlined"
+          />
+          <TextField
+            id="works"
+            label="Trabalhos"
+            multiline
+            rows={5}
+            variant="outlined"
+          />
+          <TextField
+            id="social-networks"
+            label="Como acompanhar sua história?"
+            placeholder="ex.: LinkedIn, Instagram, Github, etc"
+            multiline
+            rows={3}
+            variant="outlined"
+          />
+        </form>
+      </main>
+    </div>
   </div>
 )
 
