e68615bfe7d8dc00f438dc86eeca816165cf3efd
Fernanda Duarte <fernanda.aduarte@hotmail.com>
2019-03-11 21:06:57 -0300

    Change "Be part of it" form

--- src/pages/bePartOfIt/index.jsx
+++ src/pages/bePartOfIt/index.jsx
@@ -1,17 +1,15 @@
 import React from "react"
-// import axios from 'axios'
+import axios from 'axios'
 import PropTypes from "prop-types"
-import classNames from "classnames"
 import TextField from "@material-ui/core/TextField"
 import { withStyles } from "@material-ui/core/styles"
 import Input from "@material-ui/core/Input"
 import InputLabel from "@material-ui/core/InputLabel"
 import MenuItem from "@material-ui/core/MenuItem"
 import FormControl from "@material-ui/core/FormControl"
-import ListItemText from "@material-ui/core/ListItemText"
 import Select from "@material-ui/core/Select"
-import Checkbox from "@material-ui/core/Checkbox"
-import Chip from "@material-ui/core/Chip"
+import Button from '@material-ui/core/Button'
+import Chip from '@material-ui/core/Chip'
 
 import style from "./style.module.scss"
 import Header from "../../components/header"
@@ -23,8 +21,7 @@ const styles = theme => ({
     flexWrap: "wrap"
   },
   formControl: {
-    margin: theme.spacing.unit,
-    width: '100%'
+    marginTop: 10
   },
   chips: {
     display: "flex",
@@ -35,11 +32,16 @@ const styles = theme => ({
   },
   noLabel: {
     marginTop: theme.spacing.unit * 3
-  }
-});
+  },
+  button: {
+    marginTop: 20,
+    marginBottom: 20,
+    width: 200
+  },
+})
 
-const ITEM_HEIGHT = 48;
-const ITEM_PADDING_TOP = 8;
+const ITEM_HEIGHT = 48
+const ITEM_PADDING_TOP = 8
 const MenuProps = {
   PaperProps: {
     style: {
@@ -47,47 +49,35 @@ const MenuProps = {
       width: 250
     }
   }
-};
+}
 
-const areas = [
-  "Oliver Hansen",
-  "Van Henry",
-  "April Tucker",
-  "Ralph Hubbard",
-  "Omar Alexander",
-  "Carlos Abbott",
-  "Miriam Wagner",
-  "Bradley Wilkerson",
-  "Virginia Andrews",
-  "Kelly Snyder"
-]
-
-function getStyles(name, that) {
-  return {
+const getStyles = (name, that) =>
+  ({
     fontWeight:
-      that.state.name.indexOf(name) === -1
+      that.state.selectedAreas.indexOf(name) === -1
         ? that.props.theme.typography.fontWeightRegular
         : that.props.theme.typography.fontWeightMedium
-  };
-}
+  })
+
 
 class BePartOfIt extends React.Component {
   state = {
-    name: [],
+    selectedAreas: [],
+    areas: []
   };
 
-  // async componentDidMount() {
-  //   let { data: areas } = await axios({
-  //     method: 'get',
-  //     url: '/areas',
-  //   })
-  //   // eslint-disable-next-line no-nested-ternary
-  //   areas = areas.sort((a, b) => (a.name < b.name ? -1 : a.name > b.name ? 1 : 0))
-  //   this.setState({ areas })
-  // }
+  async componentDidMount() {
+    let { data: areas } = await axios({
+      method: 'get',
+      url: '/areas',
+    })
+    // eslint-disable-next-line no-nested-ternary
+    areas = areas.sort((a, b) => (a.name < b.name ? -1 : a.name > b.name ? 1 : 0))
+    this.setState({ areas })
+  }
 
   handleChange = event => {
-    this.setState({ name: event.target.value });
+    this.setState({ selectedAreas: event.target.value })
   };
 
   handleChangeMultiple = event => {
@@ -99,12 +89,13 @@ class BePartOfIt extends React.Component {
       }
     }
     this.setState({
-      name: value
-    });
+      selectedAreas: value
+    })
   };
 
   render() {
     const { classes } = this.props
+    const { areas } = this.state
 
     return (
       <div className={style.container}>
@@ -125,79 +116,92 @@ class BePartOfIt extends React.Component {
             }
           ]}
         />
-        <div className={style.contentContainer}>
-          <main>
-            <h1 className={style.title}>Faça parte</h1>
-            <p>
+        <div className={style.center}>
+          <div className={style.contentContainer}>
+            <main>
+              <h1 className={style.title}>Faça parte</h1>
+              <p>
               Quer ver o seu perfil publicado no BitUs? Conte-nos sobre você no
               formulário abaixo!
-            </p>
-            <form noValidate autoComplete="off" className={style.formContainer}>
-              <TextField id="name" label="Nome" className={style.input} />
-              <TextField
-                id="location"
-                label="Localização"
-                className={style.input}
-              />
-              <TextField
-                id="location"
-                label="Educação"
-                placeholder="ex.: Faculdade, pós-graduacão, cursos, etc"
-                className={style.input}
-              />
-              <FormControl className={classes.formControl}>
-                <InputLabel htmlFor="select-multiple-checkbox">Tag</InputLabel>
-                <Select
-                  multiple
-                  value={this.state.name}
-                  onChange={this.handleChange}
-                  input={<Input id="select-multiple-checkbox" />}
-                  renderValue={selected => selected.join(", ")}
-                  MenuProps={MenuProps}
-                >
-                  {areas.map(name => (
-                    <MenuItem key={name} value={name}>
-                      <Checkbox checked={this.state.name.indexOf(name) > -1} />
-                      <ListItemText primary={name} />
-                    </MenuItem>
-                  ))}
-                </Select>
-              </FormControl>
-              <TextField
-                id="about"
-                label="Sobre"
-                multiline
-                rows={5}
-                variant="outlined"
-                className={style.textarea}
-              />
-              <TextField
-                id="works"
-                label="Trabalhos"
-                multiline
-                rows={5}
-                variant="outlined"
-                className={style.textarea}
-              />
-              <TextField
-                id="social-networks"
-                label="Como acompanhar sua história?"
-                placeholder="ex.: LinkedIn, Instagram, Github, etc"
-                multiline
-                rows={3}
-                variant="outlined"
-                className={style.textarea}
-              />
-            </form>
-          </main>
+              </p>
+              <form noValidate autoComplete="off" className={style.formContainer}>
+                <TextField id="name" label="Nome" className={style.input} />
+                <TextField
+                  id="location"
+                  label="Localização"
+                  className={style.input}
+                />
+                <TextField
+                  id="location"
+                  label="Educação"
+                  placeholder="ex.: Faculdade, pós-graduacão, cursos, etc"
+                  className={style.input}
+                />
+                <FormControl className={classes.formControl}>
+                  <InputLabel htmlFor="select-multiple-chip">Áreas</InputLabel>
+                  <Select
+                    multiple
+                    value={this.state.selectedAreas}
+                    onChange={this.handleChange}
+                    input={<Input id="select-multiple-chip" />}
+                    renderValue={selected => (
+                      <div className={classes.chips}>
+                        {selected.map(value => (
+                          <Chip key={value} label={value} className={classes.chip} />
+                        ))}
+                      </div>
+                    )}
+                    MenuProps={MenuProps}
+                  >
+                    {areas.map(area => (
+                      <MenuItem key={area._id} value={area.name} style={getStyles(area.name, this)}>
+                        {area.name}
+                      </MenuItem>
+                    ))}
+                  </Select>
+                </FormControl>
+                <TextField
+                  id="about"
+                  label="Sobre"
+                  multiline
+                  rows={5}
+                  variant="outlined"
+                  className={style.textarea}
+                />
+                <TextField
+                  id="works"
+                  label="Trabalhos"
+                  multiline
+                  rows={5}
+                  variant="outlined"
+                  className={style.textarea}
+                />
+                <TextField
+                  id="social-networks"
+                  label="Como acompanhar sua história?"
+                  placeholder="ex.: LinkedIn, Instagram, Github, etc"
+                  multiline
+                  rows={5}
+                  variant="outlined"
+                  className={style.textarea}
+                />
+                <div className={style.buttonContainer}>
+                  <Button variant="contained" color="primary" className={classes.button}>
+                    Enviar
+                  </Button>
+                </div>
+              </form>
+            </main>
+          </div>
         </div>
+
       </div>
-    );
+    )
   }
 }
 
 BePartOfIt.propTypes = {
   classes: PropTypes.object.isRequired
-};
+}
 
-export default withStyles(styles, { withTheme: true })(BePartOfIt);
+export default withStyles(styles, { withTheme: true })(BePartOfIt)
